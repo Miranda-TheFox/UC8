a9272ef4abf79a8258806345f751b2cd2573783d
DAVI BRANDAO MIRANDA VAZ <davivazprofissional@gmail.com>
2026-08-12 10:47:33 -0300

    Add files via upload

---
+++ missao_112/missao_111/js/script.js
@@ -0,0 +1,29 @@
+//seleciona o botão do gravador e o elemento de status
+const botaoGravador = document.getElementById("btn-gravador");
+
+//seleciona o elemento de status
+const statusGravacao = document.getElementById("status-gravacao");
+
+//adiciona o evento de clique ao botão do gravador
+botaoGravador.addEventListener("pointerdown", () => {
+    statusGravacao.textContent = "Status: Capturando áudio...";
+    //altera a cor e o texto do botão para indicar que está gravando
+    botaoGravador.style.backgroundColor = "#e74c3c";
+    botaoGravador.textContent = "🔴 Gravando... Não solte!";
+});
+
+//adiciona o evento de soltar o botão do gravador
+botaoGravador.addEventListener("pointerup", () => {
+    statusGravacao.textContent = "Status: Gravação concluída e enviada!";
+    //altera a cor e o texto do botão para indicar que a gravação foi concluída
+    botaoGravador.style.backgroundColor = "#3498db";
+    botaoGravador.textContent = "🎤 Clique e Segure para Gravar";
+});
+
+//adiciona o evento de cancelar a gravação
+botaoGravador.addEventListener("pointercancel", () => {
+    statusGravacao.textContent = "Status: Gravação cancelada!";
+    //altera a cor e o texto do botão para indicar que a gravação foi cancelada
+    botaoGravador.style.backgroundColor = "#747777";
+    botaoGravador.textContent = "Gravação cancelada. Clique e Segure para Gravar";
+});
